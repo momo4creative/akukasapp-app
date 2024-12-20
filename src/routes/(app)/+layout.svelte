5e3bb760cf8fd95e3e9f470eaf4825268a677e0f
harie-hunt <haripuji8@gmail.com>
2024-12-20 20:26:32 +0700

    page home

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { LayoutData } from './$types';
+
+	let { data, children }: { data: LayoutData; children: Snippet } = $props();
+</script>
+
+<div>
+	<span>{data.user?.username}</span>
+	<a href="/auth/logout">Logout</a>
+</div>
+
+{@render children()}
