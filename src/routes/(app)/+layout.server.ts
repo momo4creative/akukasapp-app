b125d3300ea750ed1068f43905d6f3b52552bee2
harie-hunt <haripuji8@gmail.com>
2024-12-22 23:58:43 +0700

    fix layout

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,5 +1,13 @@
+import { akun, transaksi } from '$lib/server/app';
 import type { LayoutServerLoad } from './$types';
 
 export const load = (async ({ locals: { user } }) => {
-    return { user };
+    return {
+        user,
+        promiseAkun: akun.read(),
+        promiseLastTransaksi: transaksi.read({
+            _sort: { created_at: 'desc' },
+            _order: [0, 1]
+        })
+    };
 }) satisfies LayoutServerLoad;
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,20 +1,28 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 	import type { LayoutData } from './$types';
+	import { setAkuns, setPromiseLastTransaksi } from '$lib/utils/context';
 
 	let { data, children }: { data: LayoutData; children: Snippet } = $props();
+	setAkuns(data.promiseAkun);
+	setPromiseLastTransaksi(data.promiseLastTransaksi);
 </script>
 
 <div class="container mx-auto px-3 text-right">
 	<span>{data.user?.username}</span>
-	<a href="/auth/logout">Logout</a>
+	<a
+		href="/auth/logout"
+		class="inline-block rounded border border-red-600 px-1.5 text-sm text-red-600 hover:bg-red-600 hover:text-white"
+		>Logout</a
+	>
 </div>
 
-<header class="bg-white px-3">
-	<div class="container mx-auto flex">
+<header class="bg-white">
+	<div class="container mx-auto flex px-3">
 		<a href="/" class="font-bold">AKUNKAS-APP</a>
 		<nav class="px-3">
 			<a href="/akun">Akun</a>
+			<a href="/transaksi">Transaksi</a>
 		</nav>
 	</div>
 </header>
