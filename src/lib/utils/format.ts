f10101ff9976542c34d8d475ad6c1082b00e5714
harie-hunt <haripuji8@gmail.com>
2024-12-22 23:57:55 +0700

    fix format

--- src/lib/utils/format.ts
+++ src/lib/utils/format.ts
@@ -1,3 +1,13 @@
 export function formatNumberToRupiah(value: string | number) {
     return new Intl.NumberFormat('id', { style: 'currency', currency: 'IDR', maximumFractionDigits: 0 }).format(Number(value))
 }
+
+export function formatOptionsSelect(val: Record<string, any>[]): [string, string][] {
+    return val.map(d => [d.kode + " - " + d.uraian, d.id])
+}
+
+
+export function formatDateInput(val: string | Date) {
+    const d = new Date(val)
+    return new Intl.DateTimeFormat('fr-CA').format(d)
+}
