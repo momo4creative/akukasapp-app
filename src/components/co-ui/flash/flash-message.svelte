eb95ff5176af7d41a1051c2cf52b6fbfe37f3dc4
harie-hunt <haripuji8@gmail.com>
2024-12-23 21:59:11 +0700

    fix need auth

--- src/components/co-ui/flash/flash-message.svelte
+++ src/components/co-ui/flash/flash-message.svelte
@@ -3,15 +3,31 @@
 	import Icon from '@iconify/svelte';
 	import cn from '$lib/utils/cn';
 	import { fade } from 'svelte/transition';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/state';
 
 	interface Props extends HTMLButtonAttributes {
-		message?: string;
-		status?: 'error' | 'success' | 'warning';
+		message?: string | null;
+		statusTxt?: 'error' | 'success' | 'warning' | string | null;
+		status?: 400 | 401 | 404 | number;
 	}
-	let { message, status = 'error' }: Props = $props();
+	let { message, statusTxt = 'error', status }: Props = $props();
 
-	function getIcon(status: Props['status']) {
-		switch (status) {
+	$effect(() => {
+		const msg = page.url.searchParams.get('msg');
+		const sts = page.url.searchParams.get('statusTxt');
+		if (msg) message = msg;
+		if (sts) statusTxt = sts;
+	});
+
+	if (status == 401) {
+		console.log('need auth');
+
+		goto('/auth/login?msg=Silakan login terlebih dahulu !&statusTxt=warning');
+	}
+
+	function getIcon(statusTxt: Props['statusTxt']) {
+		switch (statusTxt) {
 			case 'error':
 				return 'alert-circle-outline';
 			case 'success':
@@ -28,12 +44,12 @@
 	<div
 		transition:fade
 		class={cn('flex gap-1.5 rounded p-1 text-white', {
-			'bg-red-500': status == 'error',
-			'bg-green-500': status == 'success',
-			'bg-yellow-400 text-inherit': status == 'warning'
+			'bg-red-500': statusTxt == 'error',
+			'bg-green-500': statusTxt == 'success',
+			'bg-yellow-400 text-inherit': statusTxt == 'warning'
 		})}
 	>
-		<Icon icon="mdi:{getIcon(status)}" width="24" height="24" />
+		<Icon icon="mdi:{getIcon(statusTxt)}" width="24" height="24" />
 		<p class="px-1">{message}</p>
 	</div>
 {/if}
--- src/routes/(app)/transaksi/+page.svelte
+++ src/routes/(app)/transaksi/+page.svelte
@@ -4,8 +4,12 @@
 	import List from '@pages/transaksi/list.svelte';
 	import Pagination from '@pages/layout/pagination.svelte';
 	import Memuat from '@ui/loading/memuat.svelte';
+	import type { ResultHandleFail } from '$lib/utils/handle-action';
+	import FlashMessage from '@ui/flash/flash-message.svelte';
 
 	let { data }: { data: PageData } = $props();
+
+	data.promiseListTransaksi.then((v) => v).catch((err) => console.log(err));
 </script>
 
 <h1 class="m-3 text-4xl">Transaksi</h1>
@@ -15,8 +19,8 @@
 {:then res}
 	<List values={res.data} />
 	<Pagination values={res} />
-{:catch err}
-	<p>{err.message}</p>
+{:catch err: ResultHandleFail}
+	<FlashMessage message={err.message} status={err.status} />
 {/await}
 
 <div class="m-3 rounded bg-white p-3">
